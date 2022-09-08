916dadeb2b478aa482e1036156059257c4c0ba5a
yakovleval <sanya89_muravlenko@mail.ru>
2022-09-09 00:06:10 +0300

    1.8 first attempt

--- hw1/1.8/main.c
+++ hw1/1.8/main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#define MAX_LENGTH 256
+
+int zerosNumber(const int *array) {
+    int count = 0;
+    for (int i = 0; array[i] != -1; i++) {
+        if (array[i] == 0) {
+            count++;
+        }
+    }
+    return count;
+}
 
 int main() {
-    printf("Hello, World!\n");
+    printf("введите через пробел элементы массива (целые числа, -1 -- конец массива)\n");
+    int array[MAX_LENGTH] = {0};
+    int i = 0;
+    int read = 0;
+    do {
+        scanf("%d", &read);
+        array[i] = read;
+        i++;
+    } while (read != -1);
+    printf("количество нулей в массиве равно %d\n", zerosNumber(array));
     return 0;
 }
