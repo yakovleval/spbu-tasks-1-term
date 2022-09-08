3d3a4b6f843af66c002851278498a8b872cd3262
yakovleval <sanya89_muravlenko@mail.ru>
2022-09-08 23:02:47 +0300

    1.5 first attempt

--- hw1/1.5/main.c
+++ hw1/1.5/main.c
@@ -1,6 +1,49 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+bool isSameType(const char leftBrace, const char rightBrace) {
+    return leftBrace == '(' && rightBrace == ')' ||
+    leftBrace == '[' && rightBrace == ']' ||
+    leftBrace == '{' && rightBrace == '}' ||
+    leftBrace == '<' && rightBrace == '>';
+}
+
+bool areBracketsBalanced(const char *brackets) {
+    char stack[strlen(brackets) + 1];
+    int topIndex = 0; // points to first free index
+    for (int i = 0; i < strlen(brackets); i++) {
+        switch (brackets[i]) {
+            case '(':
+            case '[':
+            case '{':
+            case '<':
+                stack[topIndex] = brackets[i];
+                topIndex++;
+                break;
+            case ')':
+            case ']':
+            case '}':
+            case '>':
+                if (topIndex == 0 ||
+                    !isSameType(stack[topIndex - 1], brackets[i])) {
+                    return false;
+                }
+                topIndex--;
+                break;
+            default:
+                return false;
+        }
+    }
+    return topIndex == 0;
+}
 
 int main() {
-    printf("Hello, World!\n");
+    char brackets[256] = {0};
+    printf("введите последовательность из скобок (), [], {} или <>: \n");
+    scanf("%255s", brackets);
+    printf(areBracketsBalanced(brackets) ?
+    "скобочная последовательность правильная\n" :
+    "скобочная последовательность неправильная\n");
     return 0;
 }
