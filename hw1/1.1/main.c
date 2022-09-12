6991293a99151a681ab6cdc7feccd45e67aa7852
yakovleval <sanya89_muravlenko@mail.ru>
2022-09-12 20:53:48 +0300

    change int with float

--- hw1/1.1/main.c
+++ hw1/1.1/main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-long long evaluatePolynom(int x) {
-    long long firstMultiplication = x * x;
+float evaluatePolynom(float x) {
+    float firstMultiplication = x * x;
     return firstMultiplication * (firstMultiplication + x) + firstMultiplication + x + 1;
 }
 
 int main() {
-    int x = 0;
-    printf("Введите целое число: ");
+    float x = 0;
+    prfloatf("Введите целое число: ");
     scanf("%d", &x);
-    printf("Результат равен %lld\n", evaluatePolynom(x));
+    prfloatf("Результат равен %lld\n", evaluatePolynom(x));
     return 0;
 }
