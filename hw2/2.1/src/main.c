b57299d39193ad1b4979ae376d57e47ddb50a0ca
yakovleval <sanya89_muravlenko@mail.ru>
2022-09-30 16:41:56 +0300

    fix negative numbers

--- hw2/2.1/src/main.c
+++ hw2/2.1/src/main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include "solution.h"
+#include <locale.h>
 
 int main() {
-    for (int i = 10; i < 45; i++) {
+    setlocale(LC_ALL, "RU-ru");
+    for (int i = -10; i < 45; i++) {
         printf("номер числа Фибоначчи: %d\n", i);
         printf("итеративно: %d\n", iterativeFibonacci(i));
         printf("рекурсивно: %d\n", recursiveFibonacci(i));
--- hw2/2.1/tests/tests.c
+++ hw2/2.1/tests/tests.c
@@ -3,15 +3,23 @@
 #include <stddef.h>
 
 void TestFibonacci(CuTest *tc) {
-    int fibonacciNumbers[] = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144};
-    for (int i = 0; i < 12; i++) {
-        CuAssertIntEquals(tc, fibonacciNumbers[i], iterativeFibonacci(i + 1));
+    int  PosFibonacciNumbers[] = {0,1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144};
+    int NegFibonacciNumbers[] = {0, 1, -1, 2, -3, 5, -8, 13};
+    for (int i = 0; i < sizeof(PosFibonacciNumbers) / sizeof(int); i++) {
+        CuAssertIntEquals(tc, PosFibonacciNumbers[i], iterativeFibonacci(i));
+    }
+    for (int i = 0; i < sizeof(NegFibonacciNumbers) / sizeof(int); i++) {
+        CuAssertIntEquals(tc, NegFibonacciNumbers[i], iterativeFibonacci(-i));
     }
 }
 
 void TestRecursiveFibonacci(CuTest *tc) {
-    int fibonacciNumbers[] = {1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144};
-    for (int i = 0; i < 12; i++) {
-        CuAssertIntEquals(tc, fibonacciNumbers[i], recursiveFibonacci(i + 1));
+    int PosFibonacciNumbers[] = {0,1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144};
+    int NegFibonacciNumbers[] = {0, 1, -1, 2, -3, 5, -8, 13};
+    for (int i = 0; i < sizeof(PosFibonacciNumbers) / sizeof(int); i++) {
+        CuAssertIntEquals(tc, PosFibonacciNumbers[i], recursiveFibonacci(i));
+    }
+    for (int i = 0; i < sizeof(NegFibonacciNumbers) / sizeof(int); i++) {
+        CuAssertIntEquals(tc, NegFibonacciNumbers[i], recursiveFibonacci(-i));
     }
 }
