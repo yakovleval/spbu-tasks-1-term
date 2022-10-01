43233c16e4582a5368e8e04414e80857634f08a4
yakovleval <sanya89_muravlenko@mail.ru>
2022-10-01 18:37:52 +0300

    task 3.2

--- hw3/3.2/main.c
+++ hw3/3.2/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <locale.h>
 #include <stdbool.h>
+#include <stdlib.h>
+
+#define TEST_ARRAY_SIZE 50
 
 // sorts segment [leftBound, rightBound] in array
 void insertionSort(int *array, const int leftBound, const int rightBound) {
@@ -72,8 +75,99 @@ void quickSort(int *array, const int leftBound, const int rightBound) {
     quickSort(array, leftPartIndex, rightBound);
 }
 
+bool binSearch(const int *array, const int size, const int key) {
+    // leftBound <= key, rightBound > key
+    int leftBound = 0;
+    int rightBound = size;
+    while (rightBound - leftBound > 1) {
+        int middle = (leftBound + rightBound) / 2;
+        if (array[middle] <= key) {
+            leftBound = middle;
+        } else {
+            rightBound = middle;
+        }
+    }
+    return array[leftBound] == key;
+}
+
+bool doesExist(const int *array, const int size, const int key) {
+    for (int i = 0; i < size; i++) {
+        if (array[i] == key) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool testBinSearch(const int *array, const int size, const int key) {
+    return doesExist(array, size, key) == binSearch(array, size, key);
+}
+
+void generateRandomArray(int *array, const int size) {
+    for (int i = 0; i < size; i++) {
+        array[i] = rand() % TEST_ARRAY_SIZE;
+    }
+}
+
+bool isPassed() {
+    int testArray[TEST_ARRAY_SIZE] = {0};
+    int testKeys[TEST_ARRAY_SIZE] = {0};
+    generateRandomArray(testArray, TEST_ARRAY_SIZE);
+    generateRandomArray(testKeys, TEST_ARRAY_SIZE);
+
+    quickSort(testArray, 0, TEST_ARRAY_SIZE - 1);
+    bool result = true;
+    for (size_t i = 0; i < TEST_ARRAY_SIZE; i++) {
+        result &= testBinSearch(testArray, TEST_ARRAY_SIZE, testKeys[i]);
+    }
+    return result;
+}
+
+
 int main() {
     setlocale(LC_ALL, "RU-ru");
-    printf("Hello, World!\n");
+    srand(239);
+
+    if (!isPassed()) {
+        printf("тесты не пройдены\n");
+        return 0;
+    }
+
+    printf("введите размер массива и кол-во случайных элементов (n и k): \n");
+    int n = 0, k = 0;
+    scanf("%d%d", &n, &k);
+    while (n < 0 || k < 0) {
+        printf("n и k должны быть неотрицательными\n");
+        printf("введите размер массива и кол-во случайных элементов (n и k): \n");
+        scanf("%d%d", &n, &k);
+    }
+    int *randomArray = calloc(n, sizeof(int));
+    int *randomNumbers = calloc(k, sizeof(int));
+
+    for (int i = 0; i < n; i++) {
+        randomArray[i] = rand() % n;
+    }
+    for (int i = 0; i < k; i++) {
+        randomNumbers[i] = rand() % n;
+    }
+
+    quickSort(randomArray, 0, n - 1);
+
+    printf("массив:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", randomArray[i]);
+    }
+    printf("\n");
+
+    for (int i = 0; i < k; i++) {
+        if (binSearch(randomArray, n, randomNumbers[i])) {
+            printf("%d -- содержится в массиве\n", randomNumbers[i]);
+        } else {
+            printf("%d -- не содержится в массиве\n", randomNumbers[i]);
+        }
+    }
+
+    free(randomNumbers);
+    free(randomArray);
     return 0;
 }
