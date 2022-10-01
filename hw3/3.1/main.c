d8486ac2c14a8b7f8dc39e20e2bce07275f7bc26
yakovleval <sanya89_muravlenko@mail.ru>
2022-10-01 15:00:16 +0300

    task 3.1

--- hw3/3.1/main.c
+++ hw3/3.1/main.c
@@ -1,8 +1,134 @@
 #include <stdio.h>
 #include <locale.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <memory.h>
+
+#define TEST_ARRAYS_NUMBER 10
+#define ARRAYS_SIZE 50
+
+// sorts segment [leftBound, rightBound] in array
+void insertionSort(int *array, const int leftBound, const int rightBound) {
+    for (int i = leftBound + 1; i <= rightBound; i++) {
+        for (int j = i - 1; j >= leftBound && array[j] > array[j + 1]; j--) {
+            const int tmp = array[j];
+            array[j] = array[j + 1];
+            array[j + 1] = tmp;
+        }
+    }
+}
+
+// parts segment [leftBound, rightBound] of array
+// returns right bound of left part and left bound of right part
+void partition(int *array, const int leftBound, const int rightBound,
+               int *pLeftIndex, int *pRightIndex, const int pivot) {
+
+    int leftIndex = leftBound;
+    int rightIndex = rightBound;
+    while (leftIndex <= rightIndex) {
+        while (leftIndex <= rightBound && array[leftIndex] < pivot) {
+            leftIndex++;
+        }
+        while (rightIndex >= leftBound && array[rightIndex] >= pivot) {
+            rightIndex--;
+        }
+        if (leftIndex <= rightIndex) {
+            const int tmp = array[leftIndex];
+            array[leftIndex] = array[rightIndex];
+            array[rightIndex] = tmp;
+            leftIndex++;
+            rightIndex--;
+        }
+    }
+    *pLeftIndex = leftIndex;
+    *pRightIndex = rightIndex;
+}
+
+// sorts segment [leftBound, rightBound] in array
+void quickSort(int *array, const int leftBound, const int rightBound) {
+    const int blockSize = rightBound - leftBound + 1;
+    if (blockSize <= 0) {
+        return;
+    }
+    if (blockSize < 10) {
+        insertionSort(array, leftBound, rightBound);
+        return;
+    }
+
+    //chooses pivot
+    int pivot = array[leftBound];
+    bool areAllEqual = true;
+    for (int i = leftBound; i < rightBound; i++) {
+        if (array[i] != array[i + 1]) {
+            pivot = array[i] > array[i + 1] ? array[i] : array[i + 1];
+            areAllEqual = false;
+            break;
+        }
+    }
+
+    if (areAllEqual) {
+        return;
+    }
+
+    int leftPartIndex = 0;
+    int rightPartIndex = 0;
+    partition(array, leftBound, rightBound, &leftPartIndex, &rightPartIndex, pivot);
+    quickSort(array, leftBound, rightPartIndex);
+    quickSort(array, leftPartIndex, rightBound);
+}
+
+bool isEqual(const int *leftArray, const int *rightArray, const int size) {
+    for (int i = 0; i < size; i++) {
+        if (leftArray[i] != rightArray[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool testQuickSort(int *array, const int size) {
+    int *sortedArray = calloc(size, sizeof(int));
+    memcpy(array, sortedArray, size * sizeof(int));
+    insertionSort(sortedArray, 0, size - 1);
+    quickSort(array, 0, size - 1);
+    bool result = isEqual(sortedArray, array, size);
+    free(sortedArray);
+    return result;
+}
+
+void generateRandomArray(int *array, const int size) {
+    for (int i = 0; i < size; i++) {
+        array[i] = rand();
+    }
+}
+
+bool testRandomArrays() {
+    int *testArrays[TEST_ARRAYS_NUMBER] = {NULL};
+    for (int i = 0; i < TEST_ARRAYS_NUMBER; i++) {
+        testArrays[i] = calloc(ARRAYS_SIZE, sizeof(int));
+        generateRandomArray(testArrays[i], ARRAYS_SIZE);
+    }
+
+    bool result = true;
+    for (int i = 0; i < TEST_ARRAYS_NUMBER; i++) {
+        result &= testQuickSort(testArrays[i], ARRAYS_SIZE);
+    }
+    for (int i = 0; i < TEST_ARRAYS_NUMBER; i++) {
+        free(testArrays[i]);
+    }
+    return result;
+}
+
+bool isPassed() {
+    return testRandomArrays();
+}
 
 int main() {
+    srand(239);
     setlocale(LC_ALL, "RU-ru");
-    printf("Hello, World!\n");
-    return 0;
+    if (!isPassed()) {
+        printf("тесты не пройдены\n");
+        return 0;
+    }
+    printf("тесты пройдены\n");
 }
