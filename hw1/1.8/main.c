4710ebf0f37d3dbee3f737f6fa5147d40476b740
yakovleval <sanya89_muravlenko@mail.ru>
2022-10-02 17:46:11 +0300

    delete redundant function

--- hw1/1.8/main.c
+++ hw1/1.8/main.c
@@ -1,18 +1,6 @@
 #include <stdio.h>
 #include <locale.h>
 
-#define MAX_LENGTH 256
-
-int zerosNumber(const int *array) {
-    int count = 0;
-    for (int i = 0; array[i] != -1; i++) {
-        if (array[i] == 0) {
-            count++;
-        }
-    }
-    return count;
-}
-
 int main() {
     setlocale(LC_ALL, "RU-ru");
     printf("введите через пробел элементы массива (целые числа, -1 -- конец массива)\n");
