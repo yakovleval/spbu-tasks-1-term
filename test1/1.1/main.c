2c1be9822345e4756e7fc8bd061ce35eb097d690
yakovleval <sanya89_muravlenko@mail.ru>
2022-10-05 15:37:46 +0300

    first version

--- test1/1.1/main.c
+++ test1/1.1/main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include <locale.h>
 
+void insertionEvenSort(int *array, const int leftBound, const int rightBound) {
+    for (int i = leftBound + 1; i <= rightBound; i++) {
+        if (array[i] % 2 == 1) {
+            continue;
+        }
+        int nextEven = i;
+        for (int j = i - 1; j >= leftBound; j--) {
+            if (array[j] % 2 == 1 || array[j] <= array[i]) {
+                continue;
+            }
+            const int tmp = array[j];
+            array[j] = array[nextEven];
+            array[nextEven] = tmp;
+            nextEven = j;
+        }
+    }
+}
+
 int main() {
     setlocale(LC_ALL, "RU-ru");
     printf("Hello, World!\n");
