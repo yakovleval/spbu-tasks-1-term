ee2d1bec4e53a04489e9de73d52a286417d39ed8
yakovleval <sanya89_muravlenko@mail.ru>
2022-10-05 15:43:44 +0300

    copypaste tests

--- test1/1.1/main.c
+++ test1/1.1/main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <locale.h>
+
+#define TEST_ARRAYS_NUMBER 10
+#define ARRAYS_SIZE 10
 
 void insertionEvenSort(int *array, const int leftBound, const int rightBound) {
     for (int i = leftBound + 1; i <= rightBound; i++) {
@@ -17,6 +21,34 @@ void insertionEvenSort(int *array, const int leftBound, const int rightBound) {
             nextEven = j;
         }
     }
+}
+
+void generateRandomEvenArray(int *array, const int size) {
+    for (int i = 0; i < size; i++) {
+        array[i] = rand();
+        array[i] = array[i] % 2 == 1 ? array[i] + 1 : array[i];
+    }
+}
+
+bool testRandomArrays() {
+    int *testArrays[TEST_ARRAYS_NUMBER] = {NULL};
+    for (int i = 0; i < TEST_ARRAYS_NUMBER; i++) {
+        testArrays[i] = calloc(ARRAYS_SIZE, sizeof(int));
+        generateRandomEvenArray(testArrays[i], ARRAYS_SIZE);
+    }
+
+    bool result = true;
+    for (int i = 0; i < TEST_ARRAYS_NUMBER; i++) {
+        result &= testQuickSort(testArrays[i], ARRAYS_SIZE);
+    }
+    for (int i = 0; i < TEST_ARRAYS_NUMBER; i++) {
+        free(testArrays[i]);
+    }
+    return result;
+}
+
+bool isPassed() {
+    return testRandomArrays();
 }
 
 int main() {
