4af4e72e0a58e6c978813310ff3636a19889682e
yakovleval <sanya89_muravlenko@mail.ru>
2022-10-22 23:26:25 +0300

    6.1

--- hw6/6.1/main.c
+++ hw6/6.1/main.c
@@ -1,8 +1,34 @@
 #include <stdio.h>
 #include <locale.h>
+#include <stdbool.h>
+#include "list.h"
 
 int main() {
     setlocale(LC_ALL, "RU-ru");
-    printf("Hello, World!\n");
+    List *list = NULL;
+    while (true) {
+        printf("введите команду:\n");
+        int command = 0;
+        scanf("%d", &command);
+        Value value;
+        switch (command) {
+            case 0:
+                freeList(list);
+                return 0;
+            case 1:
+                printf("введите значение:\n");
+                scanf("%d", &value);
+                addValue(&list, value);
+                break;
+            case 2:
+                printf("введите значение:\n");
+                scanf("%d", &value);
+                deleteValue(&list, value);
+                break;
+            case 3:
+                printList(list);
+                break;
+        }
+    }
     return 0;
 }
