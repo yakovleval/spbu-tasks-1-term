0731b1aa96cf821f5984a26ce97385faada2e611
yakovleval <sanya89_muravlenko@mail.ru>
2022-10-28 16:28:37 +0300

    7.1

---
+++ hw7/7.1/tree.h
@@ -0,0 +1,26 @@
+#ifndef INC_7_1_TREE_H
+#define INC_7_1_TREE_H
+
+typedef struct Node Node;
+typedef int Key;
+typedef char *Value;
+typedef const char *ConstValue;
+typedef int Error;
+
+// очищает память выделенную под дерево
+void freeTree(Node *root);
+
+// поиск вершины по ключу, возвращает NULL если ключа нет
+Node **findNode(Node **root, Key key);
+
+// поиск значения по ключу, возвращает NULL если ключа нет
+Value findValue(Node *root, Key key);
+
+// добавляет ключ в дерево, меняет значение существующего
+// если ключ уже есть
+Error addNode(Node **root, Key key, ConstValue value);
+
+// удаляет ключ из дерева, ничего не делает если ключа нет
+void deleteNode(Node **root, int key);
+
+#endif //INC_7_1_TREE_H
